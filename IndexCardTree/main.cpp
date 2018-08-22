dee5d8117f06a89678d1d5a9bf5780219a7098c7
Richie Burch <r.burch@gmail.com>
2018-08-22 09:42:38 -0500

    Initial Commit

---
+++ IndexCardTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IndexCardTree
+//
+//  Created by Richie Burch on 8/22/18.
+//  Copyright © 2018 TIOOP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
