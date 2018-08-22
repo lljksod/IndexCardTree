9377bbeaf8de2cb13955bc80ed877eff058428d5
Richie Burch <r.burch@gmail.com>
2018-08-22 14:08:53 -0500

    get header and implementation file ready

---
+++ IndexCardTree/BinarySearchTree.cpp
@@ -0,0 +1,9 @@
+//
+//  BinarySearchTree.cpp
+//  IndexCardTree
+//
+//  Created by Richie Burch on 8/22/18.
+//  Copyright © 2018 TIOOP. All rights reserved.
+//
+
+#include "BinarySearchTree.hpp"
---
+++ IndexCardTree/BinarySearchTree.hpp
@@ -0,0 +1,37 @@
+//
+//  BinarySearchTree.hpp
+//  IndexCardTree
+//
+//  Created by Richie Burch on 8/22/18.
+//  Copyright © 2018 TIOOP. All rights reserved.
+//
+// File: BinarySearchTree.hpp
+// Class Provided: templated binary search tree(part of the namespace burch_binsearchtree)
+//
+// TYPEDEFS and MEMBER CONSTANTS for the list class:
+//
+// CONSTRUCTORS
+//
+// CONSTANT MEMBER FUNCTIONS
+//
+// MODIFICATION MEMBER FUNCTIONS
+//
+// NONMEMBER FUNCTIONS
+//
+
+#ifndef BinarySearchTree_hpp
+#define BinarySearchTree_hpp
+
+#include <stdio.h>
+
+namespace burch_binsearchtree
+{
+    template<typename Type>
+    class BinarySearchTree
+    {
+        
+    };
+}
+
+
+#endif /* BinarySearchTree_hpp */
